d1d29f800e61bcdbab0db3bc6de53188c08e3300
Ken Mueller <kenmueller0@gmail.com>
2020-04-04 19:47:58 -0700

    graph

--- public/scott-kitchen-counter/index.js
+++ public/scott-kitchen-counter/index.js
@@ -2,17 +2,25 @@ const firestore = firebase.firestore()
 
 const doc = firestore.doc('counters/scott-kitchen')
 
-const countLabel = document.querySelector('body > .container > .box > h1')
-const incrementButton = document.querySelector('body > .container > .box > button')
+const countLabel = document.querySelector('body > .container > .inner-container > .box > h1')
+const incrementButton = document.querySelector('body > .container > .inner-container > .box > button')
+
+const chartSelector = 'body > .container > .inner-container > .chart'
+
+let count = 0
+let lastDay = null
+let logs = []
 
 let currentRotation = 0
 
 doc.onSnapshot(
 	snapshot => {
-		const count = snapshot.get('value')
+		count = snapshot.get('value')
+		lastDay = snapshot.get('lastDay')?.toDate()
+		logs = snapshot.get('logs')
 		
-		if (typeof count === 'number')
-			updateCount(count)
+		updateCount()
+		updateChart()
 	},
 	error => {
 		alert(error.message)
@@ -20,12 +28,36 @@ doc.onSnapshot(
 	}
 )
 
-const updateCount = count => {
+const updateCount = () => {
 	countLabel.innerHTML = count
 	incrementButton.style.transform = `rotate(${currentRotation += 360}deg)`
 }
 
-const incrementCount = () =>
-	doc.update({
-		value: firebase.firestore.FieldValue.increment(1)
+const updateChart = () =>
+	new Chartist.Line(
+		chartSelector,
+		{
+			labels: [...Array(logs.length).keys()],
+			series: [{ data: logs }]
+		},
+		{ showLabel: false }
+	)
+
+const incrementCount = () => {
+	const now = new Date
+	
+	return doc.update({
+		value: firebase.firestore.FieldValue.increment(1),
+		lastDay: now,
+		logs: datesAreOnSameDay(now, lastDay)
+			? logs.length
+				? (logs[logs.length - 1]++, logs)
+				: [1]
+			: firebase.firestore.FieldValue.arrayUnion(1)
 	})
+}
+
+const datesAreOnSameDay = (a, b) =>
+	a.getFullYear() === b.getFullYear() &&
+	a.getMonth() === b.getMonth() &&
+	a.getDate() === b.getDate()
